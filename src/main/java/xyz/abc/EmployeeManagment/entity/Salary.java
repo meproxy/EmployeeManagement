e307f3674d232d93f8f552942eb64495c6f3591c
Prasant Kumar <exploringcosmos@gmail.com>
2024-12-27 22:17:29 +0530

    Application Files YM

---
+++ src/main/java/xyz/abc/EmployeeManagment/entity/Salary.java
@@ -0,0 +1,16 @@
+package xyz.abc.EmployeeManagment.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Salary {
+
+}
